d63140fb03365d769552f3a18878bc6a0afd9423
Carmine Basso <147887212+cbasso1@users.noreply.github.com>
2023-11-28 12:44:21 -0500

    Got each loop the randomize the computer choice

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let compScore = 0;
+
 const getComputerChoice = () => {
   const arrOfChoices = ['rock', 'paper', 'scissors']
   const randomNum = Math.floor(Math.random() * 3);
@@ -6,6 +9,7 @@ const getComputerChoice = () => {
   getComputerChoice();
 
   const playRound = (playerSelection, computerSelection) => {
+    console.log('1 ', playerSelection, '2 ', computerSelection)
     if (playerSelection === 'rock' && computerSelection === 'rock'){
       return 'Tie! You both picked rock';
     } else if (playerSelection === 'scissors' && computerSelection === 'scissors'){
@@ -26,7 +30,18 @@ const getComputerChoice = () => {
       return 'Win! Scissors beats paper';
     }
   }
-  const playerSelection = "rock";
-  const computerSelection = getComputerChoice();
-  console.log(playRound(playerSelection, computerSelection));
+
+  const playerSelection = prompt("Do you choose rock, paper, or scissors?");
+  
+
+
+const game = () => {
+  for (let i = 0; i < 5; i++){
+    const computerSelection = getComputerChoice();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+}
+game ();
+  
+
     
